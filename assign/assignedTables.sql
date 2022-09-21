145cc424c623b93386be277a252097b225811a1f
YousefAly0 <y.aly@tildetech.ae>
2022-09-21 17:14:54 +0200

    updated assignedTables queries

--- assign/assignedTables.sql
+++ assign/assignedTables.sql
@@ -116,3 +116,50 @@ SELECT TOP (1000) [bas_assigned_user_id]
   FROM [Do2romStage23112021].[ph].[ph_brand_assigned_user]
   WHERE bas_assigned_user_id = 23131
   --DELETE FROM ph.[ph_brand_assigned_user] WHERE bas_assigned_user_id = 23131
+
+
+
+  --user 23137
+
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [mas_assigned_w_user_id]
+      ,[mas_admin_w_user_id]
+      ,[mas_market_id]
+  FROM [Do2romStage23112021].[ph].[ph_market_assigned_user]
+  WHERE mas_assigned_w_user_id = 23137
+
+  --DELETE FROM ph.[ph_market_assigned_user] WHERE mas_assigned_w_user_id = 23137 
+  /****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [las_assigned_user_id]
+      ,[las_admin_user_id]
+      ,[las_location_id]
+  FROM [Do2romStage23112021].[ph].[ph_location_assigned_user]
+  WHERE las_assigned_user_id = 23137
+  --DELETE FROM ph.[ph_location_assigned_user] WHERE las_assigned_user_id = 23137
+
+  /****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [itas_assigned_user_id]
+      ,[itas_admin_user_id]
+      ,[itas_type_id]
+      ,[MarketId]
+  FROM [Do2romStage23112021].[ph].[ph_item_type_assigned_user]
+  WHERE itas_assigned_user_id = 23137
+  --DELETE FROM ph.[ph_item_type_assigned_user] WHERE [itas_assigned_user_id] = 23137
+
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [ItemTypeAssignedUserItemTypeId]
+      ,[ItemTypeAssignedUserAssignedUserId]
+      ,[ItemTypeAssignedUserMarketId]
+      ,[BrandId]
+  FROM [Do2romStage23112021].[ph].[AssignedItemTypeBrands]
+  WHERE ItemTypeAssignedUserAssignedUserId = 23137
+  --DELETE FROM ph.[AssignedItemTypeBrands] WHERE [ItemTypeAssignedUserAssignedUserId] = 23137
+
+  /****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [bas_assigned_user_id]
+      ,[bas_admin_user_id]
+      ,[bas_brand_id]
+      ,[MarketId]
+  FROM [Do2romStage23112021].[ph].[ph_brand_assigned_user]
+  WHERE bas_assigned_user_id = 23137
+  --DELETE FROM ph.[ph_brand_assigned_user] WHERE bas_assigned_user_id = 23137
