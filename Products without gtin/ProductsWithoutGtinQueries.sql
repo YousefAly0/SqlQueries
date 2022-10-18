f51bede2a0a65f20f9ee892cc872ded4e4fca936
YousefAly0 <y.aly@tildetech.ae>
2022-10-18 14:18:02 +0200

    added queries

--- Products without gtin/ProductsWithoutGtinQueries.sql
+++ Products without gtin/ProductsWithoutGtinQueries.sql
@@ -1,8 +1,40 @@
 
 --Products that have price and image and allowed to diplay
 
+--in stock
+SELECT DISTINCT prd.ep_id AS Id, prd.ep_arabicName AS Name, prd.ep_Barcode AS Gtin 
+FROM excelproducts prd
+INNER JOIN excelproducts_images img ON img.ep_img_epid = prd.ep_id                                
+INNER JOIN market_products_prices price ON price.markpr_pr_id = prd.ep_id
+WHERE  (prd.ep_disable_pr_inapp  <> 1 OR prd.ep_disable_pr_inapp IS NULL) AND ep_Barcode IS NULL 
+	
+
+-- out of stock
 SELECT DISTINCT prd.ep_id AS Id, prd.ep_arabicName AS Name, prd.ep_Barcode AS Gtin 
 FROM excelproducts prd
 INNER JOIN excelproducts_images img ON img.ep_img_epid = prd.ep_id                                
 --INNER JOIN market_products_prices price ON price.markpr_pr_id = prd.ep_id
-WHERE  (prd.ep_disable_pr_inapp  <> 1 OR prd.ep_disable_pr_inapp IS NULL) AND ep_Barcode IS NULL
+WHERE  (prd.ep_disable_pr_inapp  <> 1 OR prd.ep_disable_pr_inapp IS NULL) AND ep_Barcode IS NULL 
+	AND prd.ep_id NOT IN( SELECT markpr_pr_id
+		FROM market_products_prices)
+
+
+--Products that have price and image and not allowed to diplay
+
+--in stock
+SELECT DISTINCT prd.ep_id AS Id, prd.ep_arabicName AS Name, prd.ep_Barcode AS Gtin 
+FROM excelproducts prd
+INNER JOIN excelproducts_images img ON img.ep_img_epid = prd.ep_id                                
+INNER JOIN market_products_prices price ON price.markpr_pr_id = prd.ep_id
+WHERE  (prd.ep_disable_pr_inapp  = 1 OR prd.ep_disable_pr_inapp IS not NULL) AND ep_Barcode IS NULL 
+	
+
+-- out of stock
+SELECT DISTINCT prd.ep_id AS Id, prd.ep_arabicName AS Name, prd.ep_Barcode AS Gtin 
+FROM excelproducts prd
+INNER JOIN excelproducts_images img ON img.ep_img_epid = prd.ep_id                                
+--INNER JOIN market_products_prices price ON price.markpr_pr_id = prd.ep_id
+WHERE  prd.ep_disable_pr_inapp  = 1  AND ep_Barcode IS NULL 
+	AND prd.ep_id NOT IN( SELECT markpr_pr_id
+		FROM market_products_prices)
+
